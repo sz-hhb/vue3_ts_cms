bb7e46ea74972f229b823b5a1457da04b0345275
Jackey Huang <binhuang@tuofaninfo.com>
2023-03-04 16:59:40 +0800

    feat(vue.config.js): update vue config js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: "@/components",
+      },
+    },
+  },
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     views: '@/views'
+  //   }
+  // },
+  // chainWebpack: (config) => {
+  //   config.resolve.alias
+  //     .set("@", path.resolve(__dirname, "src"))
+  //     .set("components", "@/components");
+  // },
 });
